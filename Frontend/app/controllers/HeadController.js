878c459db553f3e8e4a614695a68ecfa4fbcb862
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-07-29 02:03:28 -0300

    Configurar Rotas de Direcionamento para Login -> Dashboard e Logout -> Login

--- Frontend/app/controllers/HeadController.js
+++ Frontend/app/controllers/HeadController.js
@@ -7,35 +7,40 @@ angular.module('app')
         $rootScope.profile_image = AuthService.getUserDetails().imageUrl;
         $rootScope.nome_usuario = AuthService.getUserDetails().name;
         $rootScope.isLogged = true;
+       
     } else{
         $rootScope.profile_image = "/../../images/profile.png";
         $rootScope.nome_usuario = "Visitante";
-        $rootScope.isLogged = false ;
+        $rootScope.isLogged = false;
+      
     }
+
 
 
     $rootScope.logout = function(){
         AuthService.logout();
     }
-
-    $rootScope.login = function(){
-        
-    }
-    
-     
+         
        $rootScope.$on('event:social-sign-in-success', function(event, userDetails){
             AuthService.setUserDetails(userDetails);
             $rootScope.profile_image = AuthService.getUserDetails().imageUrl;
             $rootScope.nome_usuario = AuthService.getUserDetails().name;
             $rootScope.isLogged = true;
+            $location.path("/dashboard");
             $rootScope.$apply();
-   
+            
+
        });
 
        $rootScope.$on('event:social-sign-out-success', function(event, logoutStatus){
         $rootScope.profile_image = "/../../images/profile.png";
         $rootScope.nome_usuario = "Visitante";
+        $location.path("/login");
         $rootScope.isLogged = false;
+       
+        
+
+    
        })
     });
 
