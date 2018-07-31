4c310cd532d80b299d8cfd254b851e843cbdc8b6
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-07-31 20:09:06 -0300

    User data model

--- Frontend/app/controllers/UserDataController.js
+++ Frontend/app/controllers/UserDataController.js
@@ -1,12 +1,30 @@
 angular.module('app')
     .controller('UserDataController',  function($rootScope, $location,AuthService)
     {
+
+        const vm = this;
        $rootScope.activetab = $location.path();
        $rootScope.user_email = AuthService.getUserDetails().email;
-       $rootScope.matricula_usuario = 11210403;
-
+       vm.matricula_usuario = 0;
+     
        $rootScope.send = function send(){
-           console.log('mandou');
+           console.log(vm.matricula_usuario);
+           console.log(validaMatricula($rootScope.matricula_usuario));
+       }
+
+
+       function validaMatricula(matricula){
+           if (matricula.toString().length != 8) {
+               console.log(matricula.toString().length);
+               console.log('aqui');
+               return false;
+       }
+        else if(isNaN(matricula)) {
+                return false
+            }
+                else return true;
+
+
        }
      
 
