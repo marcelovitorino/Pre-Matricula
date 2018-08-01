61e2a6917d695154fd580c8266fb89f5857971f6
marcelovitorino <marcelo.vitorino@ccc.ufcg.edu.br>
2018-07-31 21:23:19 -0300

    cadastra aluno/controller ok// verificacoes ok

--- Frontend/app/controllers/SignUpController.js
+++ Frontend/app/controllers/SignUpController.js
@@ -1,9 +1,22 @@
 angular.module('app')
-    .controller('SignUpController',  function($rootScope, $location,AuthService)
+    .controller('SignUpController',  function($scope, $location,AuthService)
     {
         
 
+        $scope.sendSignUp = function() {
+                if (!validaMatricula()) {
+                    //erro
+                    
+                } else {
+                    // POST
+                }
+          }
 
+          function validaMatricula() {
+            if (isNaN($scope.numeroMatricula)) {
+                return false;
+            }   
+        }
       
 
     });
