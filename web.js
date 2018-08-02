bc0cbf6c2050bd2475a28443c59dca3b39f8ba44
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-08-02 01:19:08 -0300

    Add Deploy things

---
+++ web.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/')); //aqui você define onde está o index.html da sua aplicação.
+app.listen(process.env.PORT || 3000);
