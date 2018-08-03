da9eac752778e3060b51c05889475d54f9b757d1
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-08-03 03:40:41 -0300

    fix

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 ﻿var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/dist')); //aqui você define onde está o index.html da sua aplicação.
+app.use(express.static(__dirname + '/')); //aqui você define onde está o index.html da sua aplicação.
 app.get('/', function(req, res){
   res.redirect('/index.html');
 });
