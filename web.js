69deba8f58bdbf7962f9c6811e283a8ee97d9c78
caiosbl <caio.lira@ccc.ufcg.edu.br>
2018-08-02 22:52:05 -0300

    Fix Server

--- web.js
+++ web.js
@@ -1,7 +1,4 @@
 ﻿var express = require('express');
 var app = express();
-//app.use(express.static(__dirname + '/')); //aqui você define onde está o index.html da sua aplicação.
-app.get('/', function(req, res){
-  res.redirect('/index.html');
-});
+app.use(express.static(__dirname + '/')); //aqui você define onde está o index.html da sua aplicação.
 app.listen(process.env.PORT || 3000);
